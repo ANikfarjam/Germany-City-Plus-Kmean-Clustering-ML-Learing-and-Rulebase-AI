854cb3682ad5ae1d264b63425256fc41dee600de
Ashkan Nikfarjam <ashkan_nikfarjam@yahoo.com>
2024-03-23 18:54:06 -0700

    navbar works now

--- Analysis.py
+++ Analysis.py
@@ -1,31 +1,8 @@
 #Author: Ashkan Nikfarjam
-from dash import Dash, dcc, html
-import dash_bootstrap_components as dbc #a library and package to create nav bar and other components
 import plotly.express as px
 import pandas as pd
-import json
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("About", href="Home.py")),
-        dbc.NavItem(dbc.NavLink("Analysis", href="Analysis.py")),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("More pages", header=True),
-                dbc.DropdownMenuItem("something", href="#"),
-                dbc.DropdownMenuItem("something", href="#"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
-    ],
-    brand="Germany ---",
-    brand_href="#",
-    color="dark",
-    dark=True,
-)
+
 
 HealthCare_data = pd.read_csv("./DATA/Helthcare/numberofHospitals.csv")
 HealthCare_data.rename(columns={'City':'city'}, inplace=True)
@@ -33,9 +10,6 @@ HealthCare_data.rename(columns={'City':'city'}, inplace=True)
 city_df = pd.read_csv("./DATA/German cities.csv")
 merged_df = pd.merge(city_df, HealthCare_data, on='city')
 
-# Read GeoJSON data
-with open("./DATA/de.json", "r") as f:
-    geojson_data = json.load(f)
 
 # Create choropleth map figure
 fig = px.scatter_mapbox(merged_df, 
@@ -49,11 +23,3 @@ fig = px.scatter_mapbox(merged_df,
                         height=1000
                         )
 
-app.layout = html.Div([
-    navbar,  # Include the navbar here
-    html.Div(id='map', style={'width': '100%'}),
-    dcc.Graph(figure=fig)
-])
-
-if __name__=='__main__':
-    app.run_server(debug=True)
--- Home.py
+++ Home.py
@@ -1,14 +1,16 @@
 #Author: Ashkan Nikfarjam
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc #a library and package to create nav bar and other components
 import plotly.express as px
+import Analysis
 
-
+with open("intro.txt",'r') as f:
+    intro_text = f.read()
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Home", href="Home")),
-        dbc.NavItem(dbc.NavLink("Analysis", href="Analysis")),
+        dbc.NavItem(dbc.NavLink("Home", href="/", active='exact')),
+        dbc.NavItem(dbc.NavLink("Analysis", href="/Analysis", active='exact')),
         dbc.DropdownMenu(
             children=[
                 dbc.DropdownMenuItem("More pages", header=True),
@@ -25,11 +27,28 @@ navbar = dbc.NavbarSimple(
     color="dark",
     dark=True,
 )
+content = html.Div(id="page-content")
+app.layout = html.Div([dcc.Location(id="url"), navbar, content])
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.P(intro_text)
+    elif pathname == "/Analysis":
+        html.Div(id='map', style={'width': '100%'}),
+        graph = dcc.Graph(figure=Analysis.fig)
+        return graph
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return html.Div(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ],
+        className="p-3 bg-light rounded-3",
+    )
 
-
-app.layout = html.Div([
-    navbar,  # Include the navbar here
-])
 
 if __name__=='__main__':
     app.run_server(debug=True)
