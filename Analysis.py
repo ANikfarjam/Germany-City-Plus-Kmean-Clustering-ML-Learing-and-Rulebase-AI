63351d280fd10a900cc0c306681d9ff1665bb3b6
Ashkan Nikfarjam <ashkan_nikfarjam@yahoo.com>
2024-04-10 01:11:07 -0700

    updated GUI

--- Analysis.py
+++ Analysis.py
@@ -2,6 +2,25 @@
 import plotly.express as px
 import pandas as pd
 
+german_cities_by_state = {
+    'Berlin': ['Berlin'],
+    'Baden-Württemberg': ['Stuttgart', 'Mannheim', 'Karlsruhe', 'Freiburg im Breisgau', 'Heidelberg', 'Ulm', 'Pforzheim', 'Reutlingen', 'Esslingen', 'Ludwigsburg', 'Ostfildern', 'Kornwestheim', 'Weingarten', 'Eppelheim', 'Asperg'],
+    'Bavaria': ['Munich', 'Nuremberg', 'Augsburg', 'Regensburg', 'Ingolstadt', 'Fürth', 'Würzburg', 'Erlangen', 'Rosenheim', 'Germering', 'Ottobrunn', 'Puchheim', 'Gröbenzell', 'Haar', 'Neubiberg'],
+    'Hamburg': ['Hamburg'],
+    'North Rhine-Westphalia': ['Cologne', 'Düsseldorf', 'Dortmund', 'Essen', 'Duisburg', 'Bochum', 'Wuppertal', 'Bielefeld', 'Bonn', 'Münster', 'Mönchengladbach', 'Gelsenkirchen', 'Aachen', 'Krefeld', 'Oberhausen', 'Hagen', 'Hamm', 'Mülheim', 'Leverkusen', 'Solingen', 'Herne', 'Neuss', 'Paderborn', 'Bottrop', 'Moers', 'Siegen', 'Gladbeck', 'Herten', 'Hilden', 'Siegburg', 'Eilendorf'],
+    'Hesse': ['Frankfurt', 'Wiesbaden', 'Darmstadt', 'Kassel', 'Hattersheim', 'Obertshausen', 'Bad Soden am Taunus', 'Eschborn'],
+    'Saxony': ['Leipzig', 'Dresden', 'Chemnitz', 'Halle'],
+    'Lower Saxony': ['Hannover', 'Braunschweig', 'Oldenburg', 'Osnabrück', 'Wolfsburg', 'Göttingen', 'Salzgitter', 'Hildesheim'],
+    'Rhineland-Palatinate': ['Mainz', 'Ludwigshafen', 'Koblenz', 'Trier', 'Kaiserslautern'],
+    'Schleswig-Holstein': ['Kiel', 'Lübeck', 'Elmshorn', 'Pinneberg', 'Wentorf bei Hamburg', 'Schenefeld', 'Glinde'],
+    'Bremen': ['Bremen'],
+    'Saxony-Anhalt': ['Magdeburg', 'Halle', 'Halle-Neustadt'],
+    'Thuringia': ['Erfurt', 'Jena'],
+    'Mecklenburg-Vorpommern': ['Rostock', 'Schwerin'],
+    'Saarland': ['Saarbrücken'],
+    'Brandenburg': ['Potsdam'],
+    'Rhineland-Palatinate': ['Kaiserslautern', 'Mainz', 'Ludwigshafen', 'Koblenz', 'Trier'],
+}
 city_df = pd.read_csv("./DATA/German cities.csv")
 #creating Rentals df for map
 rental = pd.read_csv("./DATA/Rentals/cityRentals.csv")
@@ -38,18 +57,14 @@ politics = pd.read_csv("./DATA/Politics/politics.csv")
 
 
 
+#print(school_df.head())
 #use this df for city population
 mod_city = city_df.copy()
 mod_city = mod_city.dropna()
-def scatterMap(df, color_column=None, size_column=None):
-    fig = px.scatter_mapbox(df, 
-                        lat='lat',
-                        lon='lng',
-                        size= size_column,
-                        color= color_column,
-                        mapbox_style="carto-positron",
-                        center={"lat": df['lat'].mean(), "lon": df['lng'].mean()},
-                        color_continuous_scale="YlOrRd",  # Yellow to Red color scale
-                        height=1000
-                        )
-    return fig
+
+#number of Pharmecy
+pharmacy = pd.read_csv('./DATA/Helthcare/numberOfPharmecies.csv')
+#rint("pharmecy", pharmacy.head())
+#print(city_df.head())
+pharmacy_df = pd.merge(pharmacy, city_df.copy() , on='city', how='left')
+#print(pharmacy_df.head())
